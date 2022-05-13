31115888551cb6afd4463b73d0b33a01d1e333e2
mohamad.nematizadeh <89206040+mnbvcxz09156006138@users.noreply.github.com>
2022-05-13 16:08:22 +0430

    Add files via upload

--- controller/login.php
+++ controller/login.php
@@ -1,23 +1,15 @@
 <?php
-include "model/database.php";
 $username =$_POST["username"];
-$password = $_POST["password"];
+$pessword = $_POST["pessword"];
+if ($username =="mohammad" && $pessword ="09156006138")
+{
+    $_SESSION["login_status"] = true;
+ header("Location:home");
+}
+else
+{
+    header("Location:index");
 
-$secure_password = md5($password);
+}
 
- $users_count = $db->query("SELECT*FROM users WHERE username ='$username' AND password ='$secure_password'")->num_rows
- if($users_count == 1)
- {
-     $_SESSION["login_status"] = true ;
-     $_SESSION["username"] = $username ;
-     $_SESSION["user_id"] = $user["id"];
-     header("Location: home");
-
- }else{
-     $_SESSION["message"] = "نام کاربری با گذرواژه شما صحیح نمی باشد";
-     $_SESSION["message_type"] = "error";
-
-     header("Location: index");
-
- }
 ?>
